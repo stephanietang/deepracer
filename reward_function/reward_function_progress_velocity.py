e6a62306fe2c4c77e2b8803a1bd2ba43c0d611f7
stephanietang <txj.stephanie@gmail.com>
2024-06-13 11:03:55 +0800

    update reward function

--- reward_function/reward_function_progress_velocity.py
+++ reward_function/reward_function_progress_velocity.py
@@ -21,18 +21,24 @@ class MyCar:
         is_left_of_center = params['is_left_of_center']
         distance_from_center = params['distance_from_center']
         steering_angle = params['steering_angle']
+        is_offtrack = params['is_offtrack']
+        is_crashed = params['is_crashed']
 
-        normDistance = distance_from_center/track_width     
+        normDistance = distance_from_center / track_width     
         
         # Set the minimum step that we will reward the car for high speed
         # As the begining, the higher the speed (throttle) the better, and 
         # there's almost no problem when car run max speed as start, so we
         # just reward car if they start with higher speed
-        MINIMUM_SPEEDING_STEPS = 6    
-        if (steps <= MINIMUM_SPEEDING_STEPS) and (speed>= 8.5):
-            reward = 1
+        MINIMUM_SPEEDING_STEPS = 4
+        MINIMUM_SPEED = 3
+        if (steps <= MINIMUM_SPEEDING_STEPS) and (speed>= MINIMUM_SPEED):
+            reward = 2
         else:
             reward = 1e-4
+
+        A = 4
+        B = 2
     
         # We use two previous progress parameter, so the minimum step the car
         # must run before we can calculate the reward is 3
@@ -41,30 +47,19 @@ class MyCar:
         # In other words, the faster you reach the finishing line, the higher
         # reward you'll get
         if (steps > MINIMUM_SPEEDING_STEPS):
-            reward = ((progress- self.pre_progress)*2)**2 + \
-                     ((progress- self.pre_progress2))**2
+            reward = ((progress- self.pre_progress)*A)**B + \
+                     ((progress- self.pre_progress2)*0.5*A)**B
             
         # Sometime in local training, car teleport to random location, this 
         # will lead to a huge progress reward (from 10% progress jumped to 20%
         # progress) and therefore disrupts our model. Therefore we need this 
         # to remove the teleport problem
-        if ((progress - self.pre_progress)>=5) or ((progress - self.pre_progress2)>=5):
+        if ((progress - self.pre_progress)>=3) or ((progress - self.pre_progress2)>=3) or is_offtrack or is_crashed:
             reward = 1e-4
             
         # Avoid negative reward
         if (reward <= 0):
-            reward = 1e-4
-        
-        if self.debug:
-            # Print the parameter out to console (so we can see it in local training)                
-            carString=('REW: {:3.1f}, PRO: {:5.2f}, PRE_PRO: {:5.2f}, '
-                   'PRE_PRO2: {:5.2f}, STEER:{:5.1f}, SPEED: {:3.1f}, '
-                   'IS_LEFT: {}, NORMDIST: {:.2f}, DISTANCE: {:.2f}, '
-                   ' TRACK_WIDTH: {:.2f}'.format(reward, progress, 
-                   self.pre_progress, self.pre_progress2, steering_angle, 
-                   speed, is_left_of_center, normDistance, 
-                   distance_from_center, track_width))
-            print(carString)       
+            reward = 1e-4   
         
         self.pre_progress2 = self.pre_progress
         self.pre_progress = progress        
