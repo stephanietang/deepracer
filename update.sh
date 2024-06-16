f241caab7c213115c50a05ad259424dbe6eb9576
Stephanie Tang <txj.stephanie@gmail.com>
2024-06-16 07:04:47 +0000

    update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-root_folder=~/models1/
+root_folder=~/models/
 cd $root_folder
-dir_pattern="s[0-9]*"
-matched_dirs=($(ls -d $dir_pattern))
+matched_dirs=($(ls -d s[0-9]*))
 
 # check if there is matched dirs
 if [ ${#matched_dirs[@]} -eq 0 ]; then
@@ -12,7 +11,7 @@ if [ ${#matched_dirs[@]} -eq 0 ]; then
 fi
 
 # get matched dirs
-for original_dir in $matched_dirs;
+for original_dir in "${matched_dirs[@]}";
 do
     # extract number
     dir_name="${original_dir#s}"
@@ -23,15 +22,15 @@ do
     new_dir="s$new_number"
     older_number=$((number_part - 1))
     older_dir="s$older_number"
-    echo "new_dir: $new_dir, original_dir: $original_dir, older_dir: $older_dir"
+    echo "+ new_dir: $new_dir, original_dir: $original_dir, older_dir: $older_dir"
 
-    echo "Renaming directory '$original_dir' to '$new_dir'"
+    echo "+ Renaming directory '$original_dir' to '$new_dir'"
     mv "$original_dir" "$new_dir"
 
-    echo "goto directory $root_folder/$new_dir and update run.env"
-    cd $root_folder/$new_dir
-    sed -i "s/$original_dir/$new_dir/g" run.env
-    sed -i 's/$older_dir/$original_dir/g' run.env
-
+    echo "# Updating run.env in folder $new_dir"
+    sed -i "s/$original_dir/$new_dir/g" $root_folder/$new_dir/run.env
+    echo "- updated $original_dir to $new_dir"
+    sed -i "s/$older_dir/$original_dir/g" $root_folder/$new_dir/run.env
+    echo "- updated $older_dir to $original_dir"
 done
 echo "complete."
