e7c9a921a6cd441e47aff8db1a8b5402c5bc492d
stephanietang <txj.stephanie@gmail.com>
2024-08-13 09:51:14 +0800

    update auto_run.sh

--- auto_run.sh
+++ auto_run.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 echo "+ Training time(mins): $1"
 echo "# backup model#####"
-cp -r ./deepracer/models/* ./deepracer/backup/
+models_dir=./deepracer/models
+deepracer_on_the_spot_dir=./deepracer-on-the-spot
+backup_dir=./deepracer/backup/
+base=mariox-base
+cp -r $models_dir/* $backup_dir
 j=0
-for i in $(ls ../deepracer/models);
+for i in $(ls $models_dir);
 do
-	echo "# copy files ./deepracer/models/$i/* to ./deepracer-on-the-spot/custom-files folder"
-	cp -r ./deepracer/models/$i/* ./deepracer-on-the-spot/custom-files/
-	cd ./deepracer-on-the-spot/
-	sh ./create-spot-instance.shbrew install jq mariox-base $i $1
-	#./create-standard-instance.sh mariox-base $i $1
+	echo "# copy files from $models_dir/$i/* to $deepracer_on_the_spot_dir/custom-files folder"
+	cp -r $models_dir/$i/* $deepracer_on_the_spot_dir/custom-files/
+	cd $deepracer_on_the_spot_dir
+	sh ./create-spot-instance.sh $base $i $1
+	#./create-standard-instance.sh $base $i $1
 	if [ $j -gt 0 ]; then
 		echo "+ sleep for 5 mins"
 		sleep 600
