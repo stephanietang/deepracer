56e8b2a13b2bf2d412948b830309a2dbf173a144
stephanietang <txj.stephanie@gmail.com>
2024-08-13 09:39:05 +0800

    update aug1301

--- auto_run.sh
+++ auto_run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "+ Training time(mins): $1"
 echo "# backup model#####"
-cp -r ~/models/* ~/deepracer/backup/
+cp -r ./deepracer/models/* ./deepracer/backup/
 j=0
-for i in $(ls ~/models);
+for i in $(ls ../deepracer/models);
 do
-	echo "# copy files ~/models/$i/* to ~/deepracer-on-the-spot/custom-files folder"
-	cp -r ~/models/$i/* ~/deepracer-on-the-spot/custom-files/
-	cd ~/deepracer-on-the-spot/
-	./create-spot-instance.sh mariox-base $i $1
+	echo "# copy files ./deepracer/models/$i/* to ./deepracer-on-the-spot/custom-files folder"
+	cp -r ./deepracer/models/$i/* ./deepracer-on-the-spot/custom-files/
+	cd ./deepracer-on-the-spot/
+	sh ./create-spot-instance.shbrew install jq mariox-base $i $1
 	#./create-standard-instance.sh mariox-base $i $1
 	if [ $j -gt 0 ]; then
 		echo "+ sleep for 5 mins"
